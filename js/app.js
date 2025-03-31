773e65965ff28ec4719761b48d74cbed1e8ae756
geovannaetm <geovanna.t@aluno.ifsp.edu.br>
2025-03-31 17:50:44 -0300

    Criando Materias Model e Servico

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
+const materiaService = new MateriaService()
+let materia = materiaService.getAll()
+if(materia.length === 0) {
+  materiaService.add(new MateriaModel({nome : 'Back-End 1'}))
+  materiaService.add(new MateriaModel({nome : 'Front-end 2'}))
+ materiaService.add(new MateriaModel({nome : 'Banco de Dados'}))
+  materiaService.add(new MateriaModel({nome : 'Feramentas Web'}))
+} 
 
 
-const alunoservice = new Alunoservice()
+const alunoService = new Alunoservice()
     
     
 
-const alunoView = new AlunoView(document.querySelector('[data-table-alunos'))
-const alunoController = new AlunoController(alunoservice, alunoView)
+const alunoView = new AlunoView(document.querySelector('[data-table-alunos'), materiaService.getAll()
+)
+const alunoController = new AlunoController(alunoService, alunoView)
 
 
 document.querySelector('form').addEventListener('submit', function (event){
--- js/views/Aluno.view.js
+++ js/views/Aluno.view.js
@@ -1,10 +1,10 @@
 class AlunoView {
-    constructor(table)
+    constructor(table, materias)
     {
         this.tableList = table 
         this.tableHeader = this.tableList.querySelector ('thead')
         this.tableBody = this.tableList.querySelector('tbody')
-        this.materias = ["backend_1", "frontend_2", "bancodados" , "ferramentas"]
+        this.materias = materias  
 
         this.renderHeader()
     }
@@ -13,7 +13,7 @@ class AlunoView {
         const htmlHeader = document.createElement('tr')
         htmlHeader.innerHTML = '<td>Nome</td>'
         const htmlHeaderMaterias = this.materias.map(materia => {
-            return `<td>${materia}</td>`
+            return `<td>${materia.nome}</td>`
         }).join('')
     
         htmlHeader.innerHTML += htmlHeaderMaterias
@@ -30,7 +30,7 @@ class AlunoView {
             htmlRow.innerHTML = `<td>${aluno.nome}</td>`
             let encontrado = false
             this.materias.forEach(materia => {
-                if(materia in aluno.notas) {
+                if(materia._id in aluno.notas) {
                     encontrado = true
                 }
             })
@@ -39,17 +39,17 @@ class AlunoView {
             if(encontrado){
                 this.materias.forEach(materia => {
                     htmlRow.innerHTML += `<td>
-                    ${aluno.media[materia] !== undefined ? aluno.media[materia] : 
+                    ${aluno.media[materia] !== undefined ? aluno.media[materia._id] : 
                         `<a href="edit.html?id=${aluno._id}">Incluir Notas</a>`} </td>`
-                })
+                });
             }else{
-                htmlRow.innerHTML += `td colspan="${this.materias.length}">
+                htmlRow.innerHTML += `<td colspan="${this.materias.length}">
                 <a href="edit.html?id=${aluno._id}">
                     Incluir Notas
                     </a>
                 </td>`
 
-            }
+            };
 
             this.tableBody.appendChild(htmlRow)
         })
